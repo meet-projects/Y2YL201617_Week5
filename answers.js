e116ebdb927c8c8328ca5bbd3f79bf6ced933918
diala17-meet <diala17@meet.mit.edu>
2016-11-27 19:59:06 +0200

    Update answers.js
    from mohmmad , taha , diala

--- answers.js
+++ answers.js
@@ -1,11 +1,36 @@
-///PLEASE ADD YOUR SOLUTION CODE TO PART 1 HERE
-
+var temperature = 98.6;
+var temperature = temperature+5;
+var temperature = temperature*3;
+var temperature = temperature/20;
+alert(temperature);
 ///1. VARIABLES
+var Days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+ Days_of_the_week[0]="domingo"
+ Days_of_the_week[1]="lunes"
+ Days_of_the_week[2]="martes"
+ Days_of_the_week[3]="miercoles"
+ Days_of_the_week[4]="Jueves"
+ Days_of_the_week[5]="viernes"
+ Days_of_the_week[6]="sabado"
+ alert(Days_of_the_week)///2. DATA STRUCTURES
 
-///2. DATA STRUCTURES
+var icecream ={"vanilla":'1.99',"Chocolate":'2.45',"Strawberry":'2.25'}
 
+icecream["Chocolate"]= '.99'
+delete icecream["Strawberry"]
+console.log(icecream)
 ///3. FUNCTIONS & CONDITIONAL STATEMENTS
 
+var a = {"sunday":'no',"monday":'yes',"tuesday":'yes',"wednesday":'yes',"thursday":'yes',"friday":'no',"saturday":'yes'}
+function DoIHaveTOgotoschooltoday(index) {
+for i in a {
+if (i.value == "yes"){
+		return true;
+}
+else (i.value == "no"){
+		return false;
+}
+  alert(DoIHaveTOgotoschooltoday);
 ///4.  LOOPS
 
 
