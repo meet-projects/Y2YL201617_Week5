92ab869215f83dad6f7e1099ff05325b153671d2
amos17-meet <amos17@meet.mit.edu>
2016-12-08 18:29:46 +0200

    amos

--- answers.js
+++ answers.js
@@ -1,8 +1,43 @@
 ///PLEASE ADD YOUR SOLUTION CODE TO PART 1 HERE
 
 ///1. VARIABLES
+var myString="hellow";
+undefined
+var myInteger=1;
+undefined
+var myDictionart={name:'Lorenzo'};
+undefined
+var temp=98.6
+undefined
+temp=temp+5;
+103.6
+temp=temp*3
+310.79999999999995
+temp=temp/20
+15.539999999999997
+alart(temp)
+ReferenceError: alart is not defined [Learn More]
+alert(temp)
+undefined
 
+        
 ///2. DATA STRUCTURES
+var Days_of_the_Week=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+Days_of_the_Week[0]="domingo";
+Days_of_the_Week[1]="lunes";
+Days_of_the_Week[2]="martes";
+Days_of_the_Week[3]="miercoles";
+Days_of_the_Week[4]="Jueves";
+Days_of_the_Week[5]="viernes";
+Days_of_the_Week[6]="sabado";
+console.log(Days_of_the_Week);
+var ice_cream={Vanilla : "1.99", Chocolate : "2.49", Strawberry : "2.25"};
+
+ice_cream["Chocolate"] = "0.99";
+
+ice_cream["Pistacio"]="9";
+console.log(ice_cream);
+
 
 ///3. FUNCTIONS & CONDITIONAL STATEMENTS
 
